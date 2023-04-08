4b6fd03125f1fa1e4ec4fad9fb54b834b468a48d
jeffreypearson <a02278570@usu.edu>
2023-04-08 11:58:49 -0600

    basic Login
    Fixed the basic login script to accept any username, with a specific password.

--- js/login.js
+++ js/login.js
@@ -1,4 +1,15 @@
-function validate_login() {
-    alert("Ia m working");
-    window.location.href = "dashboard.html";
-}
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username && password === "web_dev") {
+        alert("You have successfully logged in.");
+        window.location.replace("dashboard.html");
+    } else {
+        alert("Try again, but this time with a better password(web_dev)");
+    }
+})
